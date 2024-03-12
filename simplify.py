c274d880789c5b9d8f4fac2dc215888d405264cf
Andrew <hello.andrew.yan@outlook.com>
2024-03-13 00:48:49 +1100

    Tied function to while loop, now can run until FULLY simplified.

--- simplify.py
+++ simplify.py
@@ -1,8 +1,9 @@
-import numpy
+import numpy as np
 import sys
 import time
 
 ###############################################################################
+
 
 def scan_map():
     text = []
@@ -10,23 +11,24 @@ def scan_map():
         row = []
         for ch in line:
             n = ord(ch)
-            if n >= 48 and n <= 57:    # between '0' and '9'
+            if n >= 48 and n <= 57:  # between '0' and '9'
                 row.append(n - 48)
-            elif n >= 97 and n <= 122: # between 'a' and 'z'
+            elif n >= 97 and n <= 122:  # between 'a' and 'z'
                 row.append(n - 87)
-            elif ch == '.':
+            elif ch == ".":
                 row.append(0)
         text.append(row)
 
     nrow = len(text)
     ncol = len(text[0])
 
-    map = numpy.zeros((nrow,ncol),dtype=numpy.int32)
+    map = np.zeros((nrow, ncol), dtype = np.int32)
     for r in range(nrow):
         for c in range(ncol):
-            map[r,c] = text[r][c]
-    
+            map[r, c] = text[r][c]
+
     return nrow, ncol, map
+
 
 ####################################################################################################
 
@@ -45,6 +47,7 @@ RIGHT = 3
 
 ####################################################################################################
 
+
 class Map:
     """Class representing the entire grid representing the Hashi game."""
 
@@ -53,7 +56,9 @@ class Map:
         self.n_col = n_col
         self.matrix = matrix
 
+
 ####################################################################################################
+
 
 class Cell:
     """Class representing the cell nodes of the Hashi grid."""
@@ -62,8 +67,10 @@ class Cell:
         self.row = row
         self.col = col
 
+
 ####################################################################################################
-        
+
+
 class Path:
     """Class representing the path between islands on the Hashi grid."""
 
@@ -72,7 +79,9 @@ class Path:
         self.direction = direction
         self.size = size
 
+
 ####################################################################################################
+
 
 class Island(Cell):
     """Subclass of Cell, representing the island nodes of the Hashi grid."""
@@ -98,21 +107,23 @@ class Island(Cell):
             list[Cell]: A list of Cells representing the path towards its destination.
         """
 
-        navigate_vertically = (direction == TOP or direction == DOWN)
-        navigate_backwards = (direction == TOP or direction == LEFT)
+        navigate_vertically = direction == TOP or direction == DOWN
+        navigate_backwards = direction == TOP or direction == LEFT
 
         # Since this function is generalised into a single loop, the range and
         # step of the loop is determined by the directions parameter.
 
         step = -1 if navigate_backwards else 1
         start = (self.row if navigate_vertically else self.col) + step
-        end = -1 if navigate_backwards else map.n_row if direction == DOWN else map.n_col
+        end = (
+            -1 if navigate_backwards else map.n_row if direction == DOWN else map.n_col
+        )
 
         path = []
         for i in range(start, end, step):
             row = self.row if not navigate_vertically else i
             col = self.col if navigate_vertically else i
-            
+
             domain = map.matrix[row][col]
             is_bridge_vertical = domain % 2 == 0
 
@@ -121,14 +132,16 @@ class Island(Cell):
                 return None
 
             # If the path contains a parallel bridge or an empty cell. Add to potentential path.
-            if domain == EMPTY_CELL or (domain > MAXIMUM_DOMAIN and is_bridge_vertical == navigate_vertically):
-                #print("Valid path added at direction" + str(direction))
+            if domain == EMPTY_CELL or (
+                domain > MAXIMUM_DOMAIN and is_bridge_vertical == navigate_vertically
+            ):
+                # print("Valid path added at direction" + str(direction))
                 path.append(Cell(row, col))
 
             # Hits a perpendicular bridge.
             if domain > MAXIMUM_DOMAIN and is_bridge_vertical != navigate_vertically:
                 return None
-            
+
             # Stops searching when it hits an island.
             if domain >= MINIMUM_DOMAIN and domain <= MAXIMUM_DOMAIN:
                 # If no cell is found before hitting this island, then no path exists.
@@ -136,9 +149,12 @@ class Island(Cell):
                     return None
                 # If the destination cell has full connections, then
                 # this path shouldn't be considered in calculations.
-                if sum(Island(row, col, domain).get_adjacent_bridge_connections(map)) == domain:
+                if (
+                    sum(Island(row, col, domain).get_adjacent_bridge_connections(map))
+                    == domain
+                ):
                     return None
-                
+
                 return path
         # Returns nothing once it's hit the edges of the grid.
         return None
@@ -177,43 +193,51 @@ class Island(Cell):
         down = map.matrix[min(self.row + 1, map.n_row - 1)][self.col]
 
         return [
-                get_bridge_size(top) if top % 2 == 0 else 0, 
-                get_bridge_size(down) if down % 2 == 0 else 0, 
-                get_bridge_size(left) if left % 2 == 1 else 0, 
-                get_bridge_size(right) if right % 2 == 1 else 0, 
-            ]
-    
+            get_bridge_size(top) if top % 2 == 0 else 0,
+            get_bridge_size(down) if down % 2 == 0 else 0,
+            get_bridge_size(left) if left % 2 == 1 else 0,
+            get_bridge_size(right) if right % 2 == 1 else 0,
+        ]
+
     def get_restricted_domain(self, map: Map) -> int:
-        paths = self.get_adjacent_paths(map)
-        bridge_connections = self.get_adjacent_bridge_connections(map)
-        restricted_domain = self.domain
 
-        debug = ""
-
-        for i in range(4):
-            if paths[i] == None and bridge_connections[i] > 0: 
-                restricted_domain -= bridge_connections[i]
-                debug = debug + " - " + str(bridge_connections[i])
-            else:
-                debug = debug + " - 0"
-        
-        #print(str(self.domain) + debug + " = " + str(restricted_domain))
-        return restricted_domain
-    
-####################################################################################################
-
-def get_bridge_size(domain: int) -> int:
         """
         This function runs in O(1) time.
 
         Parameters:
-            domain (int): Domain of the cell.
+            map (Map): Map class representing the grid.
 
         Returns:
-            int: Bridge size with the associated node.
+            int: The domain of the island after accounting for adjacent environment.
         """
-        if domain >= 0 and domain <= MAXIMUM_DOMAIN: return 0
-        return int((domain - MAXIMUM_DOMAIN) / 2) + (1 if domain % 2 == 1 else 0)
+
+        paths = self.get_adjacent_paths(map)
+        bridge_connections = self.get_adjacent_bridge_connections(map)
+        restricted_domain = self.domain
+
+        for i in range(4):
+            if paths[i] == None and bridge_connections[i] > 0:
+                restricted_domain -= bridge_connections[i]
+
+        return restricted_domain
+
+
+####################################################################################################
+
+
+def get_bridge_size(domain: int) -> int:
+    """
+    This function runs in O(1) time.
+
+    Parameters:
+        domain (int): Domain of the cell.
+
+    Returns:
+        int: Bridge size with the associated node.
+    """
+    if domain >= 0 and domain <= MAXIMUM_DOMAIN:
+        return 0
+    return int((domain - MAXIMUM_DOMAIN) / 2) + (1 if domain % 2 == 1 else 0)
 
 
 def get_islands(map: Map) -> list[Island]:
@@ -235,11 +259,13 @@ def get_islands(map: Map) -> list[Island]:
     for row in range(map.n_row):
         for col in range(map.n_col):
             domain = map.matrix[row][col]
-            if domain == EMPTY_CELL or domain > MAXIMUM_DOMAIN: continue
+            if domain == EMPTY_CELL or domain > MAXIMUM_DOMAIN:
+                continue
             islands.append(Island(row, col, domain))
     return islands
-    
-def simplify(map: Map) -> Map:
+
+
+def simplify(map: Map) -> tuple[Map, bool]:
     """
     Given a provided Hashi map, create guaranteed bridges
     as according to the layout of the islands and existing
@@ -253,51 +279,45 @@ def simplify(map: Map) -> Map:
     Returns:
         Map: Simplified map class with guaranteed bridges applied.
     """
-
+    original = Map(map.n_row, map.n_col, np.copy(map.matrix))
     # Iterate through all the islands, determine what build steps need to be
     # executed, and queue them into a list to be run after scanning.
-    copy = map
     steps = []
 
-    for island in get_islands(copy):
-        domain = island.get_restricted_domain(copy)
-        paths = island.get_adjacent_paths(copy)
+    for island in get_islands(map):  # O(n^2)
+        domain = island.get_restricted_domain(map)
+        paths = island.get_adjacent_paths(map)
         adjacency_count = 4 - paths.count(None)
         direction = 0
 
-        #print("Node: (" + str(island.row) + ", " + str(island.col) + ")")
-        #print("\tDomain: " + str(domain))
-        #print("\tPaths: " + str(paths))
-        #print("\tAdjacency Count: " + str(adjacency_count))
         # Single island connections case.
         if domain < 3 and adjacency_count == 1:
             for path in paths:
                 if path != None:
-                    #print("\tSingle")
                     steps.append(Path(path, direction, domain))
                 direction += 1
+
         # Complete islands with maximum connections case.
         elif domain % 3 == 0 and adjacency_count == domain / 3:
             for path in paths:
                 if path != None:
-                    #print("\tComplete")
                     steps.append(Path(path, direction, 3))
                 direction += 1
+
         # Guaranteed partial connections case.
         elif domain % 3 != 0 and adjacency_count == int(domain / 3 + 1):
             for path in paths:
                 if path != None:
-                    #print("\tPartial")
                     steps.append(Path(path, direction, 1))
                 direction += 1
+
     
     # After scanning each island and their configurations apply the changes.
-    for path in steps:
-        copy = create_bridge(copy, path.path, path.direction, path.size)
-    return copy
+    for path in steps:  # O(n^3)
+        map = create_bridge(map, path.path, path.direction, path.size)
+    
+    return map, not np.array_equal(original.matrix, map.matrix)
 
-####################################################################################################
-# LEGACY SHIT
 
 def create_bridge(map: Map, path: list[Cell], direction: int, length: int) -> Map:
     """
@@ -308,11 +328,11 @@ def create_bridge(map: Map, path: list[Cell], direction: int, length: int) -> Ma
 
     This function runs in O(n) time.
 
-    Args:
-        map: Map class representing the grid.
+    Parameters:
+        map (Map): Map class representing the grid.
 
     Returns:
-        A new map with the bridge created.
+        Map: A new map with the bridge created.
     """
     copy = map.matrix
     for cell in path:
@@ -328,12 +348,12 @@ def get_bridge(direction: int, length: int) -> int:
 
     This function runs in O(1) time.
 
-    Args:
-        direction: Direction to navigate.
-        length: Length of the bridge.
+    Parameters:
+        direction (int): Direction to navigate.
+        length (int): Length of the bridge.
 
     Returns:
-        The number representing the bridge.
+        int: The number representing the bridge.
     """
 
     return (
@@ -343,15 +363,14 @@ def get_bridge(direction: int, length: int) -> int:
     )
 
 
-
 def print_map(map: Map) -> None:
     """
     Prints the entire Map object.
 
     This function runs in O(n^2) time.
 
-    Args:
-        map: Map class representing the grid.
+    Parameters:
+        map (Map): Map class representing the grid.
     """
 
     for row in range(map.n_row):
@@ -364,24 +383,14 @@ def main():
     n_row, n_col, matrix = scan_map()
     map = Map(n_row, n_col, matrix)
 
-    # Helper code to estimate runtime of solution.!
+    # Helper code to estimate runtime of solution.
     start_time = time.time()
+    result, requires_restart = simplify(map)
+    while requires_restart:
+        result, requires_restart = simplify(result)
+    print_map(result)
 
-    before = simplify(map)
-    after = simplify(before)
-    result = (before.matrix == after.matrix).all()
-
-    while not result:
-        before = simplify(after)
-        after = simplify(before)
-        result = (before.matrix == after.matrix).all()
-    
-    print_map(after)
-    
     print("\033[92mRUNTIME: %ss \033[0m" % (time.time() - start_time))
-
-    # for island in completed_nodes:
-        # print(str(island.cell.row) + ", " + str(island.cell.col))
 
 
 if __name__ == "__main__":
